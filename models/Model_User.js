821350d1392e1fdb2b219798ea961492551cca12
ignaarguello <igna.arguello2110@hotmail.com>
2022-11-09 21:39:37 -0300

    Creacion de model y script/data de users

---
+++ models/Model_User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const schema_user = new mongoose.Schema({
+    name: {type:String , require:true},
+    lastName:  {type:String , require:true},
+    photo: {type:String , require:true},
+    age: {type:String , require:true},
+    email: {type:String , require:true}, 
+    password: {type:String , require:true}, 
+    code:  {type:String , require:true},
+    verified:  {type:Boolean , require:true},
+    logged: {type:Boolean , require:true}, 
+})
+
+const model_user = mongoose.model('users', schema_user);
+
+module.exports = model_user;
