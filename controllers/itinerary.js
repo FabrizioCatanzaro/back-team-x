b0e353b80a050b274c302f8042015c74269e8dec
ignaarguello <igna.arguello2110@hotmail.com>
2022-11-12 18:52:04 -0300

    Modelo de hoteles, con su data, enviada a la base de datos

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -4,9 +4,18 @@ const Itinerary = require('../models/Itinerary')
 const controller = {
     create: async(req,res) => {
         try{
+            let new_it = await Itinerary.create(req.body)
+            res.status(201).json({
+                id: new_it._id,
+                success: true,
+                message: "Congrats! The itinerary was created with success"
+            })
+        } catch (error){
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
 
-        } catch (error) {
-            
         }
     },
     read: async(req,res) => {
@@ -29,17 +38,49 @@ const controller = {
         }
     },
     update: async(req,res) => {
+        let { id } = req.params
         try{
-
+            let oneIt = await Itinerary.findOneAndUpdate({ _id: id }, req.body, {new: true})
+            if (oneIt){
+                res.status(200).json({
+                    id: oneIt._id,
+                    success: true,
+                    message: "Well done! You have modified the itinerary"
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Oh, some info is wrong. Try again!"
+                })
+            }
         } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
 
         }
     },
     destroy: async(req,res) => {
+        let { id } = req.params
         try{
-
+            let oneIt = await Itinerary.findOneAndDelete({ _id: id })
+            if (oneIt){
+                res.status(200).json({
+                    success: true,
+                    message: "The itinerary was deleted with success"
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Couldn't find that itinerary. Try again!"
+                })
+            }
         } catch (error) {
-
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
         }
     }
 }
