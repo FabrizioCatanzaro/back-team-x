e9c90fc03afcacfd5a69c6ef5c017fb45f86e589
ignaarguello <igna.arguello2110@hotmail.com>
2022-11-14 21:05:26 -0300

    Creacion de la ruta/controlador, tipo patch, para modificar un dato especifico de un show

--- routes/show.js
+++ routes/show.js
@@ -1,8 +1,8 @@
 let router = require('express').Router()
 
-let {create, read, one} = require('../controllers/controller_show')
+let {create, read, one, update} = require('../controllers/controller_show')
 
-
+router.patch('/:id_update', update)
 router.post('/', create)
 router.get('/', read)
 router.get('/:id', one)
