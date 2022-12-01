f39299cb74b836777d9ca35a65be37ca5d95d122
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2022-12-01 02:39:14 -0300

    agregando rutas de reacciones al index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ let city = require('./city')
 let itinerary = require('./itinerary')
 let hotel = require('./hotel')
 let show = require('./show')
-
+let reaction = require('./reaction')
 
 
 router.use('/api/auth', user)
@@ -12,6 +12,7 @@ router.use('/api/cities', city)
 router.use('/api/itineraries', itinerary)
 router.use('/api/hotels',hotel)
 router.use('/api/shows', show )
+router.use('/api/reactions', reaction)
 
 
 module.exports = router;
